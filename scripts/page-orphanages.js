72d6c816fbb4da5d04780c1640c32b1d2e10297d
Michelly Hiratsuka Zorzi <michelly.hiratsuka@gmail.com>
2020-10-15 20:55:15 -0300

    criação da page-orphanages.js, adicionando mapa e animações na orphanages.html

---
+++ scripts/page-orphanages.js
@@ -0,0 +1,30 @@
+//create map
+const map = L.map('mapid').setView([-22.3190459,-49.0947524], 15);
+
+//create and add tileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+
+});
+
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./images/arrow-white.svg"></a>');
+
+//create and add marker
+L
+.marker([-22.3190459,-49.0947524], { icon })
+.addTo(map)
+.bindPopup(popup)
